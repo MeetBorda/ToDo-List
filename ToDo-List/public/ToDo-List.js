bcbde9abc493b046c14a8b3e039d734e03a82bd4
Matthew Vastarelli <30515659+MattVastarelli@users.noreply.github.com>
2018-06-03 15:01:00 -0400

    Moving towards RESTful routing

--- ToDo-List/public/ToDo-List.js
+++ ToDo-List/public/ToDo-List.js
@@ -1,4 +1,4 @@
-/*this will color all the lis in alternating colors*/
+s/*this will color all the lis in alternating colors*/
 function colorlis () {
   var lis = $("li");
 
@@ -11,65 +11,20 @@ function colorlis () {
 	}
   }
 }
-/*
-This will show the trash on hover and hide on exit
-in addition it adjusts the text indents
-*/
-function showTrash () {
-  $("li").hover(function() {
-	$(this).children().fadeIn('fast', function() {
-	});
-	$(this).css('text-indent', '0px');
-	}, function() {
-		$(this).children().fadeOut('fast', function() {
-		$(this).parent().css('text-indent', '15px');
-	  });
-	});
-}
 //--------------------------------------------------------------
 colorlis();
 
-showTrash();
-/*
 //event listener for trash
 $("span").on('click', function(event) {
-  $(this).parent().fadeOut('500', function() {
-	$(this).remove();
-	colorlis();
-  });
+ 
 });
-*/
+
 //event listener for plus
 $("h1 i").on('click', function(event) {
-  $("input").fadeToggle('500', function() {
-  });
+  
 });
 
 //event listener for li
 $("li").on('click', function(event) {
   $(this).toggleClass('toggleLine-through');;
 });
-
-/*
-//event listener to add an item
-$("input").keypress(function(event) {
-  //check for enter
-  if (event.keyCode === 13) {
-	var task = $("input").val();
-	$("input").val("");
-	//add the li
-	$("ul").append('<li><span hidden class="btn"><i class="fa fa-trash-alt"></i></span>' + task +'</li>');
-	//add the trash 
-	showTrash();
-	//color the li
-	colorlis();
-	//add new listener for trash
-	$("span").last().on('click', function(event) {
-	  $(this).parent().fadeOut('500', function() {
-		$(this).remove();
-		colorlis();
-	  });
-	});
-  }
-});
-*/
